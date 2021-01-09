64bc267f87e053b02159962f6ddc63cb371936b9
DigitalReplica <64011376+digitalreplica-llc@users.noreply.github.com>
2021-01-09 10:10:18 -0500

    Basic html pages (#8)
    * add website content
    
    Create public website files. Add deploy function to copy website to s3
    
    * save stack output to json file

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,23 +3,33 @@ import argparse
 import boto3
 import botocore
 import glob
+import json
+from pathlib import Path
 import os
 import time
 
 ##### Configuration #####
 CONFIG = {
-    "cloudformation_files": "cloudformation/*.json"
+    "cloudformation_files": "cloudformation/*.json",
+    "public_website_dir": "files/s3-public-website",
+    "output_save_location": "stack_output.json"
 }
 CLOUDFORMATION_COMPLETE_STATUSES = [
     "CREATE_COMPLETE",
     "UPDATE_COMPLETE"
 ]
+CONTENT_TYPE_MAPPING = {
+    ".html": "text/html",
+    ".js": "text/javascript"
+}
 
 ##### Deploy Class #####
 class AwsDeploy:
     def __init__(self, deployment):
         self.deployment = deployment
         self.cloudformation_client = boto3.client('cloudformation')
+        self.s3_resource = boto3.resource('s3')
+        self.outputs = []
 
     def deploy_stacks(self):
         '''
@@ -70,8 +80,13 @@ class AwsDeploy:
                 stack_id = stack_info.get('StackId')
             except botocore.exceptions.ClientError as e:
                 if str(e).endswith('No updates are to be performed.'):
-                    stack_id = None
                     print("- no updates required")
+                    # Describe the stack to get its outputs
+                    stack_info = self.cloudformation_client.describe_stacks(StackName=stack_name)
+                    self.gather_stack_output(stack_info)
+
+                    # Unset stack_id, so we don't wait for stack changes to complete
+                    stack_id = None
                 else:
                     raise
 
@@ -87,10 +102,62 @@ class AwsDeploy:
 
             # Show stack outputs
             stack_info = self.cloudformation_client.describe_stacks(StackName=stack_name)
-            for output_info in stack_info.get('Stacks')[0].get('Outputs', []):
-                print("{} : {}".format(output_info.get('OutputKey'), output_info.get('OutputValue')) )
+            self.gather_stack_output(stack_info)
         print("")
 
+    def gather_stack_output(self, stack_info):
+        '''
+        Gathers all outputs from the stack, so it can be referenced by later deployment steps.
+        :return:
+        '''
+        stack_outputs = stack_info.get('Stacks')[0].get('Outputs', [])
+        self.outputs = self.outputs + stack_outputs
+
+        # Print it
+        for output_info in stack_outputs:
+            print("{} : {}".format(output_info.get('OutputKey'), output_info.get('OutputValue')) )
+
+    def save_stack_outputs(self):
+        '''
+        Saves the stack outputs into a JSON file
+        :return:
+        '''
+        print("Saving stack outputs to {}".format(CONFIG["output_save_location"]))
+        with open(CONFIG["output_save_location"], "w") as output_file:
+            json.dump(self.outputs, output_file, indent=2)
+
+    def get_output_value(self, key):
+        '''
+        Finds the stack OutputValue for a given OutputKey
+        :return: OutputValue
+        '''
+        for output_info in self.outputs:
+            if output_info.get('OutputKey') == key:
+                return output_info.get('OutputValue')
+        return None
+
+    def sync_s3_bucket(self, bucket_name, local_dir):
+        '''
+        Syncs a local directory with a given bucket
+        :return:
+        '''
+        for file in Path(local_dir).glob('**/*'):
+            if os.path.isfile(file):
+                # Get path, relative to local_dir
+                relative_file_path = os.path.relpath(file, start=local_dir)
+                (file_base, file_ext) = os.path.splitext(file)
+
+                # Determine file type
+                file_type = 'binary/octet-stream'
+                if file_ext in CONTENT_TYPE_MAPPING:
+                    file_type = CONTENT_TYPE_MAPPING[file_ext]
+
+                print("  {} -> {} (as {})".format(file, relative_file_path, file_type))
+                self.s3_resource.meta.client.upload_file(
+                    str(file),
+                    bucket_name,
+                    relative_file_path,
+                    ExtraArgs={'ACL': 'public-read', 'ContentType':file_type})
 
 ##### Find Cloudformation files #####
 if __name__ == "__main__":
@@ -104,5 +171,12 @@ if __name__ == "__main__":
     if args.deployment:
         my_aws = AwsDeploy(args.deployment)
         my_aws.deploy_stacks()
+        my_aws.save_stack_outputs()
+
+        # Copy S3 files
+        print("")
+        public_bucket_name = my_aws.get_output_value('PublicBucketName')
+        print("Copying website files to {}".format(public_bucket_name))
+        my_aws.sync_s3_bucket(public_bucket_name, CONFIG["public_website_dir"])
     else:
         parse.print_help()
